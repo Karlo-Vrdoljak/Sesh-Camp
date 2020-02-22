03b32678c22860e8498e862a3f9e49e11f82de29
Karlo-Vrdoljak <1karlo.vrdoljak@gmail.com>
2020-02-22 22:31:44 +0100

    sesh wallpaper

--- sesh-camp-client/src/app/app.module.ts
+++ sesh-camp-client/src/app/app.module.ts
@@ -15,9 +15,20 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { FormsModule } from '@angular/forms';
 import { SidebarModule } from 'primeng/sidebar';
 import { MenubarModule } from 'primeng/menubar';
-import { NgxUiLoaderModule, NgxUiLoaderService } from  'ngx-ui-loader';
 import { Config } from 'src/environments/config';
 
+import { NgxUiLoaderModule, NgxUiLoaderService, NgxUiLoaderConfig, SPINNER, POSITION, PB_DIRECTION } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  bgsColor: '#b0b0b1',
+  fgsColor: '#b0b0b1',
+  bgsPosition: POSITION.bottomCenter,
+  bgsSize: 40,
+  bgsType: SPINNER.rectangleBounce, // background spinner type
+  fgsType: SPINNER.chasingDots, // foreground spinner type
+  pbDirection: PB_DIRECTION.leftToRight, // progress bar direction
+  pbThickness: 5, // progress bar thickness
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +46,7 @@ import { Config } from 'src/environments/config';
     RadioButtonModule,
     SidebarModule,
     MenubarModule,
-    NgxUiLoaderModule
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [
     TestService,
